479e3304bad29df9518b2fdb8d72db917ba3cbd5
adofodaniel812-boop <adofodaniel812@gmail.com>
2025-08-13 19:43:55 +0000

    Update CustomerDetailsQuery.ts

--- app/graphql/customer-account/CustomerDetailsQuery.ts
+++ app/graphql/customer-account/CustomerDetailsQuery.ts
@@ -1,7 +1,7 @@
 // NOTE: https://shopify.dev/docs/api/customer/latest/objects/Customer
 export const CUSTOMER_FRAGMENT = `#graphql
   fragment Customer on Customer {
-    id
+    id 
     firstName
     lastName
     defaultAddress {
