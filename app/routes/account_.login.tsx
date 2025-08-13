9399af853bd829fbefbd0c1d41eeab66530f43d3
adofodaniel812-boop <adofodaniel812@gmail.com>
2025-08-13 19:27:49 +0000

    Update account_.login.tsx

--- app/routes/account_.login.tsx
+++ app/routes/account_.login.tsx
@@ -2,4 +2,4 @@ import type {LoaderFunctionArgs} from '@netlify/remix-runtime';
 
 export async function loader({request, context}: LoaderFunctionArgs) {
   return context.customerAccount.login();
-}
+} 
