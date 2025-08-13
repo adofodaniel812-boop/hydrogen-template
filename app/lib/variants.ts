27bd7908326fb4d2eb24ddca6ac4ececce9705a9
adofodaniel812-boop <adofodaniel812@gmail.com>
2025-08-13 19:41:39 +0000

    Update variants.ts

--- app/lib/variants.ts
+++ app/lib/variants.ts
@@ -5,7 +5,7 @@ import {useMemo} from 'react';
 export function useVariantUrl(
   handle: string,
   selectedOptions: SelectedOption[],
-) {
+) { 
   const {pathname} = useLocation();
 
   return useMemo(() => {
